32b7f404f4247c9759b535bd8a4dc09e4d0835cd
kaoutar Sougrati <feiyukine@gmail.com>
2022-07-05 15:08:39 +0100

    trying to display information about a algodClient's account

---
+++ node_status.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const algosdk = require('algosdk');
+
+const algodClient = new algosdk.Algodv2(
+    process.env.ALGOD_TOKEN,
+    process.env.ALGOD_SERVER,
+    process.env.ALGOD_POST,
+);
+
+const main = async () => {
+    /*console.log(await algodClient.status().do());*/
+    console.log(await algodClient.accountInformation(process.env.ADDR_CREATOR).do());
+}
+
+main().catch(console.error);
